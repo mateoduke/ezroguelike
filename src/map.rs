48a8a99e86c90b6dfc2105a0bedb666bf66a19b0
Mateo Duke <mateoalexiduke@gmail.com>
2024-01-09 17:56:56 -0500

    Add player movement & render functionality

--- src/map.rs
+++ src/map.rs
@@ -10,6 +10,7 @@ pub enum TileType {
 pub fn map_idx(x: i32, y: i32) -> usize {
     ((y * SCREEN_WIDTH) + x) as usize
 }
+
 pub struct Map {
     pub tiles: Vec<TileType>,
 }
@@ -19,6 +20,22 @@ impl Map {
         Self {
             tiles: vec![TileType::Floor; NUM_TILES],
         }
+    }
+
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x < SCREEN_WIDTH && point.x >= 0 && point.y < SCREEN_HEIGHT && point.y >= 0
+    }
+
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        let map_idx = map_idx(point.x, point.y);
+        self.in_bounds(point) && self.tiles[map_idx] == TileType::Floor
+    }
+
+    pub fn try_idx(&self, point: Point) -> Option<usize> {
+        if !self.in_bounds(point) {
+            return None;
+        }
+        Some(map_idx(point.x, point.y))
     }
 
     pub fn render(&self, ctx: &mut BTerm) {
