17f35a5499923937a1643795eb138d9f8d5ace14
Mateo Duke <mateoalexiduke@gmail.com>
2024-01-26 16:46:44 -0500

    Added messages as entities

--- src/systems/random_move.rs
+++ src/systems/random_move.rs
@@ -3,14 +3,18 @@ use crate::prelude::*;
 #[system]
 #[write_component(Point)]
 #[read_component(MovingRandomly)]
-pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {
-    let mut movers = <(&mut Point, &MovingRandomly)>::query();
-    movers.iter_mut(ecs).for_each(|(pos, _)| {
+pub fn random_move(ecs: &SubWorld, commands: &mut CommandBuffer) {
+    let mut movers = <(Entity, &Point, &MovingRandomly)>::query();
+    movers.iter(ecs).for_each(|(entity, pos, _)| {
         let mut rng = RandomNumberGenerator::new();
         let desination = get_random_movement(&mut rng) + *pos;
-        if map.can_enter_tile(desination) {
-            *pos = desination;
-        }
+        commands.push((
+            (),
+            WantsToMove {
+                entity: *entity,
+                destination: desination,
+            },
+        ));
     })
 }
 
